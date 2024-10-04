92e2a153881691fa0900ab1e4f26f44e1db0bde5
maerdlina <maerdlina@gmail.com>
2024-10-04 11:23:59 +0300

    34. Reverse Linked List - Done - easy

--- Medium/1497. Check If Array Pairs Are Divisible by k/src/Main.java
+++ Medium/1497. Check If Array Pairs Are Divisible by k/src/Main.java
@@ -1,3 +1,8 @@
+import com.sun.jdi.IntegerValue;
+
+import java.lang.classfile.components.ClassPrinter;
+import java.util.*;
+
 public class Main {
     public boolean canArrange(int[] arr, int k) {
         boolean ansv = false;
@@ -9,6 +14,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(2 / 3);
+
     }
 }
