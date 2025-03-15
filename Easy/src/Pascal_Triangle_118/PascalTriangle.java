9c9d71a62c3af8b16b9338ed2f953a1f841d62ba
maerdlina <58111252+maerdlina@users.noreply.github.com>
2025-03-15 18:39:36 +0300

    Pascal Triangle done

--- Easy/src/Pascal_Triangle_118/PascalTriangle.java
+++ Easy/src/Pascal_Triangle_118/PascalTriangle.java
@@ -1,17 +1,31 @@
 package Pascal_Triangle_118;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> end = new ArrayList<List<Integer>>(numRows);
-        end.add(List.of(1));
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> end = new ArrayList<List<Integer>>();
+        List<Integer> row = new ArrayList<>();
+
+        row.add(1);
+        end.add(row);
+
+        for(int i = 1; i < numRows; i++){
+            row = new ArrayList<>();
+            int prew = 0;
+            for(int j = 0; j < i; j++){
+                int next = end.get(i-1).get(j);
+                row.add(next + prew);
+                prew = next;
+            }
+            row.add(1);
+            end.add(row);
+        }
 
         return end;
     }
     public static void main(String[] args){
-
+        System.out.println(generate(6));
     }
 }
