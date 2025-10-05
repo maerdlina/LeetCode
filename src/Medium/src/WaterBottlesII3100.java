06cffab8d87bd17127f606e94273f5fbae08b30e
maerdlina <maerdlina@gmail.com>
2025-10-05 20:26:25 +0300

    easy + medium
    tasks from daily task by: 01.10 & 02.10

---
+++ src/Medium/src/WaterBottlesII3100.java
@@ -0,0 +1,24 @@
+public class WaterBottlesII3100 {
+
+    public static int maxBottlesDrunk(int numBottles, int numExchange) {
+        int full = 0, empty = numBottles, drunk = numBottles;
+
+        while(full+empty >= numExchange){
+            if(empty < numExchange){
+                drunk += full;
+                empty += full;
+                full = 0;
+            }
+            empty-=numExchange;
+            full++;
+            numExchange++;
+        }
+        drunk+=full;
+
+        return drunk;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxBottlesDrunk(13, 6));
+    }
+}
